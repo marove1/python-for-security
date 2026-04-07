763ef0e7fb6427cf2d89b9ece70302b96a606e56
mattiaroveri <mattia.roveri1@gmail.com>
2026-04-06 20:04:17 -0400

    Ex 2

--- projects/01-failed-login-counter/ex1.py
+++ projects/01-failed-login-counter/ex1.py
@@ -1,1 +1,11 @@
+errors_file = "sample_logs/errors.log"
+errors_count = 0
+
+with open(errors_file, "r") as file:
+    for line in file:
+        if "error" in line:
+            errors_count += 1
+
+print(f"The error count is" , errors_count)
+
 
--- projects/01-failed-login-counter/failed_login_counter.py
+++ projects/01-failed-login-counter/failed_login_counter.py
@@ -1,1 +1,23 @@
+log_file = "sample_logs/auth.log"
+failed_count = 0
+ip_counts = {}
 
+with open(log_file, "r") as file:
+    for line in file:
+        if "Failed password" in line:
+            failed_count += 1
+
+            parts = line.split()
+            from_index = parts.index("from")
+            ip = parts[from_index + 1]
+
+            if ip in ip_counts:
+                ip_counts[ip] += 1
+            else:
+                ip_counts[ip] = 1
+
+print("Total failed login attempts:", failed_count)
+print()
+print("Failed attempts by IP:")
+for ip, count in ip_counts.items():
+    print(f"{ip}: {count}")
